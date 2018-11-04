7d619d0a7fd5ae21b1cd5903ae4e8f758f46d33c
sandy <sandytian2000@hotmail.com>
2018-11-04 13:18:33 -0500

    deleted extra files

--- app/reducers.js
+++ app/reducers.js
@@ -7,7 +7,6 @@ import { fromJS } from 'immutable';
 import { LOCATION_CHANGE } from 'react-router-redux';
 
 import languageProviderReducer from 'containers/LanguageProvider/reducer';
-import apiCall from 'containers/HomePage/reducer';
 
 /*
  * routeReducer
