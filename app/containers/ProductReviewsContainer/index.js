60a4076413ad4108c90358cb9b0244e9cc9afb24
sandy <sandytian2000@hotmail.com>
2018-12-10 22:31:51 -0500

    review number toggle and review filters

--- app/containers/ProductReviewsContainer/index.js
+++ app/containers/ProductReviewsContainer/index.js
@@ -5,18 +5,14 @@ import {
 import {
   Grid,
   Typography,
-  withStyles,
-	Divider,
+	withStyles,
+	Chip,
 } from '@material-ui/core';
-import {
-	List,
-} from 'immutable';
 import { compose } from 'redux';
 import PropTypes from 'prop-types';
 import {
 	makeSelectReviews,
 } from '../../selectors/product';
-import Stars from '../../components/Stars';
 import ReviewBarChart from '../../components/ReviewBarChart';
 import ReviewsGroup from '../../components/ReviewsGroup';
 
@@ -31,10 +27,42 @@ class ProductReviewsContainer extends React.PureComponent {
 		})
 	}
 
+	handleDelete = () => {
+		this.setState({
+			selectedReviewNumber: '-1',
+		})
+	}
+
   render() {
 		const {
 			reviewsData,
+			classes,
 		} = this.props;
+
+		const {
+			selectedReviewNumber
+		} = this.state;
+
+		const ReviewFilters = () => {
+			return (
+				selectedReviewNumber !== '-1'
+					?	<Grid>
+							<Chip
+								label={selectedReviewNumber}
+								onDelete={this.handleDelete}
+								className={classes.chip}
+								color="primary"
+							/>
+							<Chip
+								label='Clear All'
+								onDelete={this.handleDelete}
+								className={classes.chip}
+								color="primary"
+							/>
+						</Grid>
+				: null
+			)
+		}
   
     return (
       <Grid
@@ -49,6 +77,9 @@ class ProductReviewsContainer extends React.PureComponent {
 					grabNumber={this.setSelectedReviewNumber}
 				/>
 
+				{/* review filter chips */}
+				<ReviewFilters />
+
 				{/* review */}
 				<ReviewsGroup
 					selectedReviewNumber={this.state.selectedReviewNumber}
@@ -61,7 +92,10 @@ class ProductReviewsContainer extends React.PureComponent {
 const styles = {
   root: {
 
-  },
+	},
+	chip: {
+
+	}
 }
 
 // ProductReviewsContainer.propTypes = {
